456fcdfe21bd771e7a93fe8086de0a88e0364aa0
shiv32 <yadav.shivkumar5@gmail.com>
2017-08-31 21:00:58 +0530

    First commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap("/home/shiv/Pictures/rt.png"));
+    splash->show();
+
+    MainWindow w;
+
+    QTimer::singleShot(2500,splash,SLOT(close()));
+    QTimer::singleShot(2500,&w,SLOT(show()));
+
+   // w.show();
+
+    return a.exec();
+}
